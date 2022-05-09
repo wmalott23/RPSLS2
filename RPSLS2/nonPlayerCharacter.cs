fdebd0404612f79b7f8803d2abc53959fe9f89d7
wmalott23 <101433624+wmalott23@users.noreply.github.com>
2022-05-09 14:16:19 -0700

    Created logic to have characters choose a gesture

--- RPSLS2/nonPlayerCharacter.cs
+++ RPSLS2/nonPlayerCharacter.cs
@@ -9,5 +9,18 @@ namespace RPSLS2
     public class nonPlayerCharacter : Player
     {
 
+        public string chooseGesture()
+        {
+            List<string> choices = new List<string>();
+            choices.Add("rock");
+            choices.Add("paper");
+            choices.Add("scissors");
+            choices.Add("lizard");
+            choices.Add("spock");
+            var random = new Random();
+            int index = random.Next(choices.Count);
+            string input = choices[index];
+            return input;
+        }
     }
 }
--- RPSLS2/playerCharacter.cs
+++ RPSLS2/playerCharacter.cs
@@ -8,9 +8,27 @@ namespace RPSLS2
 {
     public class playerCharacter : Player
     {
-        public void chooseGesture()
-        {
 
+        public string chooseGesture()
+        {
+            List<string> choices = new List<string>();
+            choices.Add("rock");
+            choices.Add("paper");
+            choices.Add("scissors");
+            choices.Add("lizard");
+            choices.Add("spock");
+
+            string input = "";
+            while (!choices.Contains(input))
+            {
+                foreach (var item in choices)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Please enter a gesture from the above list");
+            input = Console.ReadLine();
+            }
+            return input;
         }
     }
 }
