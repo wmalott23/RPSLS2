ba98bf4837f15d0280a3d4cb0085aad556a0af2a
wmalott23 <101433624+wmalott23@users.noreply.github.com>
2022-05-09 12:09:50 -0700

    Created new project correctly + created Gesture variables + created rock, paper, scissors, lizard, spock

---
+++ RPSLS2/Gesture.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS2
+{
+    class Gesture
+    {
+        public string winOne;
+        public string winOnePhrase;
+        public string winTwo;
+        public string winTwoPhrase;
+    }
+}
---
+++ RPSLS2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace RPSLS2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gesture rock = new Gesture();
+            rock.winOne = "scissors";
+            rock.winOnePhrase = "Rock smashes Scissors";
+            rock.winTwo = "Lizard";
+            rock.winTwoPhrase = "Rock squishes Lizard";
+
+            Gesture paper = new Gesture();
+            paper.winOne = "rock";
+            paper.winOnePhrase = "Paper covers Rock";
+            paper.winTwo = "spock";
+            paper.winTwoPhrase = "Paper disproves Spock";
+
+            Gesture scissors = new Gesture();
+            scissors.winOne = "paper";
+            scissors.winOnePhrase = "Scissors cuts Paper";
+            scissors.winTwo = "lizard";
+            scissors.winTwoPhrase = "Scissors decapitates Lizard";
+
+            Gesture lizard = new Gesture();
+            lizard.winOne = "spock";
+            lizard.winOnePhrase = "Lizard poisons Spock";
+            lizard.winTwo = "paper";
+            lizard.winTwoPhrase = "Lizard eats Paper";
+
+            Gesture spock = new Gesture();
+            spock.winOne = "scissors";
+            spock.winOnePhrase = "Spock smashes Scissors";
+            spock.winTwo = "rock";
+            spock.winTwoPhrase = "Spock vaporizes Rock";
+
+        }
+    }
+}
